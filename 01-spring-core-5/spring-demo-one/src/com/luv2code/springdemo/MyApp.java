1d47ae05c0d97489bad50c2da5804a66e537c845
vudinhquang <vudinhquangk53@gmail.com>
2021-04-17 15:29:14 +0900

    Spring Dependency Injection - Write Some Code - Part 3

--- 01-spring-core-5/spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ 01-spring-core-5/spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		// create the object
-//		Coach theCoach = new BaseballCoach();
+		// Coach theCoach = new BaseballCoach();
 		Coach theCoach = new TrackCoach();
 		
 		// use the object
