2971d8dadee781dd198b0faa8a563d31f08e8fd1
vudinhquang <vudinhquangk53@gmail.com>
2021-04-24 15:20:19 +0900

    Bean Scopes - Write Some Code - Part 2

--- 01-spring-core-5/spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
+++ 01-spring-core-5/spring-demo-one/src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -14,6 +14,19 @@ public class BeanScopeDemoApp {
 
 		Coach alphaCoach = context.getBean("myCoach", Coach.class);
 
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("\nPointing to the same object: " + result);
+		
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+
+		System.out.println("\nMemory location for alphaCoach: " + alphaCoach + "\n");
+
+		// close the context
+		context.close();
+
 	}
 
 }
